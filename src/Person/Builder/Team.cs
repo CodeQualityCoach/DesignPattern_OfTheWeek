02f61ae9df89b7ef209f06ff4fac9b2bc87c423f
Thomas Ley <Thomas.Ley@cgi.com>
2024-03-15 08:25:37 +0100

    state pattern

--- src/Person/Builder/Team.cs
+++ src/Person/Builder/Team.cs
@@ -1,5 +1,4 @@
 ﻿using Person.Builder;
-using System.Linq;
 
 namespace Person.Builder
 {
@@ -16,48 +15,6 @@ namespace Person.Builder
             Member = member;
             WorksAtCustomerSite = worksAtCustomerSite;
             AllowHomeOffice = allowHomeOffice;
-        }
-    }
-
-    // Aufgabe 1:
-    // --------------------------------
-    // implement the hrBuilder pattern for class Team
-    // Methods “AddMember”, “AssignBoss”, “AllowHomeOffice”, “WorksAtCustomerSite” 
-    internal class TeamHrBuilder : IHrBuilder
-    {
-        private Person _boss;
-        private Person[] _member;
-        private bool _worksAtCustomerSite;
-        private bool _allowHomeOffice;
-
-        public void AddMember(Person member)
-        {
-            if (_member is null) _member = new Person[0];
-            _member = _member.Append(member).ToArray();
-        }
-
-        public void AssignBoss(Person boss)
-        {
-            _boss = boss;
-        }
-
-        public void AllowHomeOffice()
-        {
-            _allowHomeOffice = true;
-            _worksAtCustomerSite = false;
-        }
-
-        public void WorksAtCustomerSite()
-        {
-            _worksAtCustomerSite = true;
-            _allowHomeOffice = false;
-        }
-
-        public Team Build()
-        {
-            if (_boss is null) throw new System.Exception("Boss is not assigned");
-
-            return new Team(_boss, _member, _worksAtCustomerSite, _allowHomeOffice);
         }
     }
 }
---
+++ src/Person/Builder/TeamHrBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace Person.Builder
+{
+    internal class TeamHrBuilder : IHrBuilder
+    {
+        private Person _boss;
+        private Person[] _member;
+        private bool _worksAtCustomerSite;
+        private bool _allowHomeOffice;
+
+        public void AddMember(Person member)
+        {
+            if (_member is null) _member = new Person[0];
+            _member = _member.Append(member).ToArray();
+        }
+
+        public void AssignBoss(Person boss)
+        {
+            _boss = boss;
+        }
+
+        public void AllowHomeOffice()
+        {
+            _allowHomeOffice = true;
+            _worksAtCustomerSite = false;
+        }
+
+        public void WorksAtCustomerSite()
+        {
+            _worksAtCustomerSite = true;
+            _allowHomeOffice = false;
+        }
+
+        public Team Build()
+        {
+            if (_boss is null) throw new System.Exception("Boss is not assigned");
+
+            return new Team(_boss, _member, _worksAtCustomerSite, _allowHomeOffice);
+        }
+    }
+}
